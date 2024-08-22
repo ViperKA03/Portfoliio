8bb2877834d1094e26b24f01c8209e07404d2b81
Kaustubh Agrawal <kaustubhagrawal20102003@gmail.com>
2024-08-22 18:29:55 +0530

    updated bug: Resume button and fixed some links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,26 @@ import React, { useState } from 'react'
 import styles from './Navbar.module.css'
 import { getImageUrl } from '../../utils.js'
 
+import KaustubhResume from './Kaustubh_resume_final.pdf';
+
+
 const Navbar = () => {
     const [menuOpen,setMenuOpen]=useState(false);
+    const handleDownload = (e) => {
+        
+        e.preventDefault();
+    
+        
+        console.log("Resume download initiated");
+    
+        
+        const link = document.createElement("a");
+        link.href = KaustubhResume;
+        link.download = "Kaustubh_resume.pdf"; // Optional: set a custom file name
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      };
   return (
    <nav className={styles.navbar }>
     <a className={styles.title} href="/">Portfolio</a>
@@ -16,7 +34,9 @@ const Navbar = () => {
             <li><a href="#experience">Experience</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#contacts">Contact</a></li>
-            <li><a href="./Kaustubh_resume.pdf" download="Kaustubh_Resume">Resume</a></li>
+            <li><a href={KaustubhResume} onClick={handleDownload}>
+      Resume
+    </a></li>
         </ul>
     </div>
    </nav>
--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -20,7 +20,7 @@ const ProjectCard = ({
         })}
       </ul>
       <div className={styles.links}>
-        <a href={demo} className={styles.link}> Site</a>
+        <a href={demo} className={styles.link} target="_blank"> Site</a>
         <a href={source} className={styles.link}>Source</a>
       </div>
     </div>
